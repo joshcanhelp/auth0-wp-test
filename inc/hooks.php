b34bf96bfaea0551040dd735cf47d311376f3397
Josh Cunningham <josh@joshcanhelp.com>
2018-02-08 19:34:55 +0200

    hooks examples

--- inc/hooks.php
+++ inc/hooks.php
@@ -56,6 +56,19 @@ function auth0_theme_hook_wpa0_user_created ( $user_id, $email, $password, $f_na
  */
 
 /**
+ * @param WP_User|null $user - found WordPress user, null if no user was found
+ * @param stdClass $userinfo - user information from Auth0
+ *
+ * @return WP_User|null
+ */
+function auth0_theme_hook_auth0_get_wp_user( $user, $userinfo ) {
+	$found_user = get_user_by( 'email', $userinfo->email );
+	$user = $found_user instanceof WP_User ? $user : null;
+	return $user;
+}
+//add_filter( 'auth0_get_wp_user', 'auth0_theme_hook_auth0_get_wp_user', 1, 2 );
+
+/**
  * @param string $html - HTML to modify, echoed out within wp_die()
  * @param stdClass $userinfo - user info object from Auth0
  * @param string $id_token - DEPRECATED, do not use
@@ -70,14 +83,62 @@ function auth0_theme_hook_auth0_verify_email_page ( $html, $userinfo, $id_token 
 //add_filter( 'auth0_verify_email_page', 'auth0_theme_hook_auth0_verify_email_page', 10, 3 );
 
 /**
- * @param WP_User|null $user - found WordPress user, null if no user was found
- * @param stdClass $userinfo - user information from Auth0
+ * @param string $connection - name of the connection, initially pulled from Auth0 plugin settings
  *
- * @return WP_User|null
+ * @return string mixed
  */
-function auth0_theme_hook_auth0_get_wp_user( $user, $userinfo ) {
-	$found_user = get_user_by( 'email', $userinfo->email );
-	$user = $found_user instanceof WP_User ? $user : null;
+function auth0_theme_hook_auth0_get_auto_login_connection( $connection ) {
+	$connection = ! empty( $_GET[ 'connection' ] ) ? sanitize_text_field( $_GET[ 'connection' ] ) : $connection;
+	return $connection;
+}
+//add_filter( 'auth0_get_auto_login_connection', 'auth0_theme_hook_auth0_get_auto_login_connection' );
+
+/**
+ * @param mixed $value - value of the option, initially pulled from the database
+ * @param string $key - key of the settings array
+ *
+ * @return mixed
+ */
+function auth0_theme_hook_wp_auth0_get_option( $value, $key ) {
+	$value = 'bad_key' === $key ? 'That is a bad key and you know it' : $value;
+	return $value;
+}
+//add_filter( 'wp_auth0_get_option', 'auth0_theme_hook_wp_auth0_get_option', 10, 2 );
+
+/**
+ * @param WP_User $user - WordPress user object found during migration and authenticated
+ *
+ * @return WP_User
+ */
+function auth0_theme_hook_auth0_migration_ws_authenticated( $user ) {
+	$user->data->display_name = 'Sir ' . $user->data->display_name . ', Esquire';
 	return $user;
 }
-//add_filter( 'auth0_get_wp_user', 'auth0_theme_hook_auth0_get_wp_user', 1, 2 );
+//add_filter( 'auth0_migration_ws_authenticated', 'auth0_theme_hook_auth0_migration_ws_authenticated' );
+
+/**
+ * @param bool $should_create - should the user be created, initialized as TRUE
+ * @param stdClass $userinfo - Auth0 user information
+ *
+ * @return bool
+ */
+function auth0_theme_hook_wpa0_should_create_user( $should_create, $userinfo ) {
+	$should_create = FALSE === strpos( 'josh', $userinfo->email );
+	return $should_create;
+}
+//add_filter( 'wpa0_should_create_user', 'auth0_theme_hook_wpa0_should_create_user' );
+
+/**
+ * @param string $css, initialized as empty
+ *
+ * @return string
+ */
+function auth0_theme_hook_auth0_login_css( $css ) {
+	$css .= '
+		body {background: radial-gradient(#01B48F, #16214D)} 
+		#login h1 {display: none}
+		.login form.auth0-lock-widget {box-shadow: none}
+	';
+	return $css;
+}
+//add_filter( 'auth0_login_css', 'auth0_theme_hook_auth0_login_css' );
