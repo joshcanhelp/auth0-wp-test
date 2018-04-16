9f0360c54b3f4fac5ef37dbcc39fa96078cb2594
Josh Cunningham <josh@joshcanhelp.com>
2018-04-16 13:11:57 -0700

    test login TPL theme override; 404 page

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_template_part('template-parts/block', 'header'); ?>
+
+    <article>
+        <h1>404</h1>
+    </article>
+
+<?php get_template_part('template-parts/block', 'footer'); ?>
---
+++ templates/auth0-login-form.php
@@ -0,0 +1,2 @@
+<h2 style="margin-bottom: 2em; color: white; text-align: center">Custom login form:</h2>
+<div id="auth0-login-form"></div>
